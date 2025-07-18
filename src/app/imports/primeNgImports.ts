fc62e971c3619e28b2082bd7308078bf37dc3814
Akash-joy <akash202002@gmail.com>
2025-07-18 23:11:20 +0530

    Auto-generated commit

--- src/app/imports/primeNgImports.ts
+++
@@ -1,6 +0,0 @@
-import { ToggleButtonModule } from 'primeng/togglebutton';
-import { PopoverModule } from 'primeng/popover';
-export const primeNgImports = [
-  ToggleButtonModule,
-  PopoverModule
-];
