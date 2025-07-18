771742608aea4af3ddcfd041cf450583754e4aef
Akash-joy <akash202002@gmail.com>
2025-07-19 00:14:05 +0530

    changes

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  mapUrl: 'https://api.openrouteservice.org/v2/directions/driving-car/geojson' // Use HTTPS
+};
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  production: false,
   mapUrl: 'https://api.openrouteservice.org/v2/directions/driving-car/geojson'
 }
